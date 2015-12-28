847b7eea5f0b0751e59c581c26f8f66b62a0e202
SeongMin Yun <sungmin7465@gmail.com>
2015-12-28 11:03:50 +0900

    my_server is modified (first completion) and my_client is being developed

--- resource/examples/my_server.cpp
+++ resource/examples/my_server.cpp
@@ -20,18 +20,21 @@ class LightResource
 {
 
 public:
+
 	string resource_name, resource_uri, resource_typename, resource_interface;
 	bool resource_state;
-        int resource_power;
-        OCResourceHandle resource_handle;
+    int resource_power;
+    OCResourceHandle resource_handle;
 	OCRepresentation resource_rep;
 	uint8_t resource_property;
 	
 	LightResource() : resource_name("Seongmin's light"), resource_state(false),
-			  resource_power(0), resource_uri("/a/light"), resource_handle("nullptr"),
-			  resource_typename("core.light"), resource_interface(DEFAULT_INTERFACE)		
-		 	  resource_property(OC_DISCOVERABLE)
+			  resource_power(0), resource_uri("/a/light"), resource_handle(nullptr),
+			  resource_typename("core.light")
+
 	{
+		resource_interface = DEFAULT_INTERFACE;
+		resource_property = OC_DISCOVERABLE;
 		resource_rep.setUri(resource_uri);
 		resource_rep.setValue("state", resource_state);
 		resource_rep.setValue("power", resource_power);
@@ -45,7 +48,7 @@ public:
 		EntityHandler eh = bind(&LightResource::entityHandler, this, PH::_1);
 
 		OCStackResult result = OCPlatform::registerResource(resource_handle,
-		resource_uri, resource_typename, resource_interface, cb, resource_property);
+		resource_uri, resource_typename, resource_interface, eh, resource_property);
 
 		if(OC_STACK_OK != result)
 		{
@@ -53,8 +56,138 @@ public:
 		}
 	}
 
+	OCRepresentation get()
+    {
+		resource_rep.setValue("state", resource_state);
+       	resource_rep.setValue("power", resource_power);
+
+        return resource_rep;
+    }
+
+    void put(OCRepresentation& rep)
+    {
+        
+		try 
+		{
+
+			if(rep.getValue("state", resource_state))
+			{
+				cout << "\t\t\t\t" << "state: " << resource_state << endl;
+       		 }
+          
+			else
+       	 	{
+       	   		cout << "\t\t\t\t" << "state not found in the representation" << endl;
+       		}
+
+       	 	if(rep.getValue("power", resource_power))
+      	  	{
+       	 		cout << "\t\t\t\t" << "power: " << resource_power << endl;
+          	}
+           
+			else
+        	{
+        		cout << "\t\t\t\t" << "power not found in the representation" << endl;
+       	 	}
+        }
+
+    	catch(exception& e)
+    	{
+    		cout << e.what() << endl;
+    	}
+    
+	}
+
+
+private:
+
+	OCEntityHandlerResult entityHandler(std::shared_ptr<OCResourceRequest> request)
+	{
+
+		OCEntityHandlerResult ehResult = OC_EH_ERROR;
+	
+		if(request)
+		{
+
+			string requestType = request->getRequestType();
+			int requestFlag = request->getRequestHandlerFlag();
+
+        	if(requestFlag & RequestHandlerFlag::RequestFlag)		//observe 기능은 미구현이므로 ObserverFlag에 대한 처리는 안하였다.
+       	 	{
+	
+				auto pResponse = std::make_shared<OC::OCResourceResponse>();
+				pResponse->setRequestHandle(request->getRequestHandle());
+				pResponse->setResourceHandle(request->getResourceHandle());
+		
+				if(requestType == "GET")
+				{
+					//normal response로 처리하는 것임, slowresponse는 스레드를 생성하여 핸들러를 사용하는데, 이 프로그램에선 아직 사용하지 않겠다.
+					pResponse->setErrorCode(200); 
+					pResponse->setResponseResult(OC_EH_OK);
+					pResponse->setResourceRepresentation(get());
+					if(OC_STACK_OK == OCPlatform::sendResponse(pResponse))
+					{
+						ehResult = OC_EH_OK;
+					}
+				}
+
+
+				else if(requestType == "PUT")
+				{
+					OCRepresentation rep = request->getResourceRepresentation();
+					put(rep);
+					pResponse->setErrorCode(200);
+					pResponse->setResponseResult(OC_EH_OK);
+					pResponse->setResourceRepresentation(get());
+	
+					if(OC_STACK_OK == OCPlatform::sendResponse(pResponse))
+   	    	    	{
+   	   					ehResult = OC_EH_OK;
+   	         		}
+				}
+
+			}
+
+		}
+
+		else
+		{	
+			cout << "Request invalid" << endl;
+		}
+	
+		return ehResult;	
+	}
+
+};
+
+int main(int argc, char* argv[])
+{
 
 
 
+    PlatformConfig cfg 
+	{
+        OC::ServiceType::InProc,
+        OC::ModeType::Server,
+        "0.0.0.0", // By setting to "0.0.0.0", it binds to all available interfaces
+        0,         // Uses randomly available port
+        OC::QualityOfService::LowQos
+    };
+
+    OCPlatform::Configure(cfg);
+
+
+	LightResource virtual_light;
+
+	virtual_light.createResource();
+
+	cout << "virtual_light is created!\n";
+	
+	mutex blocker;
+    condition_variable cv;
+    unique_lock<std::mutex> lock(blocker);
+    cout <<"Waiting" << std::endl;
+    cv.wait(lock, []{return false;});
+
+	return 0;
 }
-
